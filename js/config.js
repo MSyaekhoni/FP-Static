89b49418652ce1ff742d364f043fcf8a388a5ed8
MSyaekhoni <syaekhonimuhammad@gmail.com>
2020-12-18 05:52:58 +0700

    change config.js

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_xUNrOTH3I',
         userPoolClientId: '743dnjk117df4u0tiqi5tv9rbv',
-        region: 'eu-west-1'
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+        invokeUrl: 'https://k2cdp5ols3.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
